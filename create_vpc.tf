4b3e71be9ae5823ad678af5451796c078999220f
MrTux <128902493+Blu-7@users.noreply.github.com>
2024-12-04 17:46:17 +0700

    X

---
+++ create_vpc.tf
@@ -0,0 +1,28 @@
+# List all avalability zones in the region
+data "aws_availability_zones" "available" {}
+locals {
+  selected_azs = slice(data.aws_availability_zones.available.names, 0, var.aws_vpc_config.number_of_availability_zones)
+}
+
+# Create VPC with public and private subnets
+module "vpc" {
+  source               = "./modules/vpc"
+  vpc_cidr             = var.aws_vpc_config.cidr_block
+  enable_dns_hostnames = var.aws_vpc_config.enable_dns_hostnames
+  enable_dns_support   = var.aws_vpc_config.enable_dns_support
+  public_subnets_cidr  = var.aws_vpc_config.public_subnets_cidr
+  private_subnets_cidr = var.aws_vpc_config.private_subnets_cidr
+  availability_zones   = local.selected_azs
+  enable_nat_gateway   = var.aws_vpc_config.enable_nat_gateway
+}
+
+# Get my public IP address
+data "http" "my_ip" {
+  url = "http://checkip.amazonaws.com"
+}
+
+module "keypair" {
+  source = "./modules/keypair"
+  name   = var.aws_project
+  algorithm = "RSA"
+}
